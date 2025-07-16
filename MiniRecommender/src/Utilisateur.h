80061af9c43b4127bb35ed9f5268813c4cbf3a7b
bits0002 <emmanuel.bitsindou@etudiant.univ-reims.fr>
2025-07-16 08:24:23 +0200

    Ajout du header Utilisateur.h - Définition de la classe Utilisateur

--- MiniRecommender/src/Utilisateur.h
+++ MiniRecommender/src/Utilisateur.h
@@ -13,15 +13,14 @@
             Utilisateur();
             Utilisateur(std::string);
 
-            void ajouter_note(std::string, int);
-            int get_note(std::string) const;
-            bool a_note_film(std::string);
+            void ajouter_note(std::string&, int);
+            int get_note(std::string&) const;
+            bool a_note_film(std::string&);
             
             std::string get_nom() const;
             std::map<std::string, int> get_notes_films() const; 
 
             double calcule_similarite(Utilisateur&) const;
-
     };
 
 #endif // __UTILISATEUR__
